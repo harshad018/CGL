ad64db7d688e465fba8b06266d67e28f60ebadc3
Harshad Ghutukade <108193335+harshad018@users.noreply.github.com>
2024-03-07 13:09:04 +0530

    assignment 4 with boundaryfill only
    need to add floodfill which is quite similar as that of boundaryfill just change bdcolor to bgcolor or vice versa

--- boundaryFill.cpp
+++ boundaryFill.cpp
@@ -1,77 +1,69 @@
 #include<iostream>
 #include<GL/glut.h>
 using namespace std;
-int x1,y1,x2,y2;
-float c[3];
-float bcolor[3] = {1.0,1.0,1.0};
-float fillColor[3] = {1.0,0.0,0.0};
+int X1,X2,Y1,Y2,x,y;
+float Color[3];
+float bgColor[3] = {1,1,1};
+float fillColor[3] = {1,0,0}; // Change this line
 void init()
 {
-	glClearColor(0,0,0,0);//Background color and foreground color of screen
-	gluOrtho2D(-320,320,-240,240);//set screen dimensions
+	glClearColor(0,0,0,0);
+	gluOrtho2D(0,640,0,480);//set screen dimensions
 }
 
-/*void drawQuadrants()//for display 4 quadrants
+void setPixel(int x,int y,float fillColor[3])
 {
-	glBegin(GL_LINES);
-		glVertex2i(-320,0);
-		glVertex2i(320,0);
-		
-		glVertex2i(0,-240 the screen
-	
-	drawQuadrants();
-	drawpolygon();
-	);                                                    
-		glVertex2i(0,240);
-	glEnd();
-	
-}
-*/
-void drawpolygon()
-{
- glBegin(GL_LINE_LOOP);
-       glVertex2i(x1,y1);
-       glVertex2i(x2,y1);
-       glVertex2i(x2,y2);
-       glVertex2i(x1,y2);
-    glEnd(); 
-  }
-    
-void getPixel(int x, int y){
- 
- 	glReadPixels(x, y, 1, 1, GL_RGB, GL_FLOAT, c);
- 
- 
-}
-
-void setPixel(int x, int y){
-
 	glColor3fv(fillColor);
 	glBegin(GL_POINTS);
-	glVertex2i(x,y);
-	glEnd();
+        	glVertex2i(x,y);
+        glEnd(); 
+        glFlush();
+}
 
-}  
+void getPixel(int x,int y,float Color[3])
+{
+	glReadPixels(x,y,1.0,1.0,GL_RGB,GL_FLOAT,Color);
+	
+}
 
-void boundaryFill(int x, int y){
+void boundaryFill(int x, int y,float fillColor[3],float bgColor[3]){
 	
-	getPixel(x,y);
 	
-	if(c[0]!=bcolor[0]&&c[1]!=bcolor[1]&&c[2]!=bcolor[2]){
-		setPixel(x,y);
-		boundaryFill(x,y+1);
-        	boundaryFill(x+1,y);
-        	boundaryFill(x,y-1);
-        	boundaryFill(x-1,y);
+	 getPixel(x,y,Color);
 	
+	
+	if((Color[0] != bgColor[0] && Color[1] != bgColor[1] && Color[2] != bgColor[2]) ){
+	
+		setPixel(x,y,fillColor);
+		boundaryFill(x+1,y,fillColor,bgColor);
+		boundaryFill(x-1,y,fillColor,bgColor);
+		boundaryFill(x,y+1,fillColor,bgColor);        	
+        	boundaryFill(x,y-1,fillColor,bgColor);
 	}
 
 }
+
+void drawPolygon(){
+glColor3fv(bgColor);
+	glBegin(GL_LINES);
+		glVertex2i(X1,Y1);
+		glVertex2i(X1,Y2);
+		glVertex2i(X1,Y2);
+		glVertex2i(X2,Y2);
+		glVertex2i(X2,Y2);
+		glVertex2i(X2,Y1);
+		glVertex2i(X2,Y1);
+		glVertex2i(X1,Y1);
+	glEnd();
+
+glFlush();
+}
 void display()
-{
+{glClearColor(0,0,0,0);//Background color and foreground color of screen
 	glClear(GL_COLOR_BUFFER_BIT);//clear
-	drawpolygon();
-	boundaryFill(150,100);
+	drawPolygon();
+	
+	boundaryFill(40,60,fillColor,bgColor);
 	
 	
 	glFlush(); 	
@@ -80,10 +72,14 @@ void display()
 int main(int argc,char **argv)
 {
    
-   	cin>>x1;
-   	cin>>y1;
-   	cin>>x2;
-   	cin>>y2;
+   	cout<<"Enter the value of X1 coordinate:"<<endl;
+	cin>>X1;
+	cout<<"Enter the value of Y1 coordinate:"<<endl;
+	cin>>Y1;
+	cout<<"Enter the value of X2 coordinate:"<<endl;
+	cin>>X2;
+	cout<<"Enter the value of Y2 coordinate:"<<endl;
+	cin>>Y2;
 	glutInit(&argc,argv);
 	glutInitWindowSize(640,480);
 	glutInitWindowPosition(100,100);
